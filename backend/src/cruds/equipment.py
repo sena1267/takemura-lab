3a8437fb7c38c7b3f1089077656577132c27c2bd
sena1267 <silc0504@gmail.com>
2023-05-21 13:47:44 +0900

    selectの書き方を修正

--- backend/src/cruds/equipment.py
+++ backend/src/cruds/equipment.py
@@ -1,24 +1,24 @@
 import models
-from schemas.equipment import (Equipment, EquipmentCreate, EquipmentUpdate)
+from schemas.equipment import Equipment, EquipmentCreate, EquipmentUpdate
 
 from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 
 
 def get_equipments(db: Session) -> list[Equipment]:
-    # sqlalchemy2.0では、execute()を使った書き方が推奨されるらしい
-    equipments = db.execute(
-        select(models.Equipment)
-    ).scalars().all()
+    # sqlalchemy2.0では、この書き方が推奨らしい。
+    # equipments = db.execute(
+    #     select(models.Equipment)
+    # ).scalars().all()
+    equipments = db.scalars(select(models.Equipment)).all()
 
     return list(map(Equipment.from_orm, equipments))
 
 
 def get_equipment_by_id(equipment_id: int, db: Session):
-    equipment = db.execute(
-        select(models.Equipment)
-        .where(models.Equipment.id == equipment_id)
-    ).scalar()
+    equipment = db.scalar(
+        select(models.Equipment).where(models.Equipment.id == equipment_id)
+    )
 
     if equipment is None:
         return None
@@ -36,25 +36,24 @@ def create_equipment(equipment: EquipmentCreate, db: Session):
 
 
 def update_equipment(equipent_id: int, equipment: EquipmentUpdate, db: Session):
-    updated_equipment = db.execute(
+    updated_equipment = db.scalar(
         update(models.Equipment)
         .where(models.Equipment.id == equipent_id)
-        .values({
-            models.Equipment.name: equipment.name,
-            models.Equipment.price: equipment.price,
-            models.Equipment.buyer_id: equipment.buyer_id,
-            models.Equipment.bought_date: equipment.bought_date
-        })
+        .values(
+            {
+                models.Equipment.name: equipment.name,
+                models.Equipment.price: equipment.price,
+                models.Equipment.buyer_id: equipment.buyer_id,
+                models.Equipment.bought_date: equipment.bought_date,
+            }
+        )
         .returning(models.Equipment)
-    ).scalar()
+    )
     db.commit()
 
     return Equipment.from_orm(updated_equipment)
 
 
 def delete_equipment(equipment_id: int, db: Session):
-    db.execute(
-        delete(models.Equipment)
-        .where(models.Equipment.id == equipment_id)
-    )
+    db.execute(delete(models.Equipment).where(models.Equipment.id == equipment_id))
     db.commit()
