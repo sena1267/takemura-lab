c2c012ceda0a6890fc76aa191415a0b8fab4082e
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-05-26 18:41:03 +0900

    complete docker

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -3,17 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
-POSTGRES_USER = os.environ.get('POSTGRES_USER', 'fastapi')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'takemuraapp1212')
-POSTGRES_SERVER = os.environ.get('POSTGRES_SERVER', 'postgres')
-POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
-POSTGRES_DB = os.environ.get('POSTGRES_DB', 'fastapi')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_SERVER = os.environ.get('POSTGRES_HOST')
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
 
-SQLALCHEMY_DATABASE_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
-    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_PORT, POSTGRES_DB
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}/{}".format(
+    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_DB
 )
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    echo=False
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
