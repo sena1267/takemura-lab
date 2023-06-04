bbe1c57013757645bbaff393af4b8b0a1cf1c198
sena1267 <silc0504@gmail.com>
2023-06-04 14:38:20 +0900

    コメント修正

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from database import database
 from fastapi import FastAPI
 from routers import (user, equipment)
-from starlette.middleware.cors import CORSMiddleware # 追加
+from starlette.middleware.cors import CORSMiddleware
 
 database.Base.metadata.create_all(bind=database.engine)
 
@@ -10,11 +10,11 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(equipment.router)
 
-# CORSを回避するために追加
+# CORSを回避する
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,   # 追記により追加
-    allow_methods=["*"],      # 追記により追加
-    allow_headers=["*"]       # 追記により追加
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
