44f98f75a3a6c55b628c7c382dbdec9d62d89cfe
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-06-10 16:09:45 +0900

    add auth front

--- frontend/src/Loginform.js
+++ frontend/src/Loginform.js
@@ -1,6 +1,8 @@
 import "./Loginform.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import qs from 'qs';
 
 const Loginform = () => {
     //ユーザー情報を格納するオブジェクトを生成する
@@ -11,21 +13,75 @@ const Loginform = () => {
     const [formErrors, setFormErrors] = useState({})
     //ログインボタンが押されたかどうかを判別するオブジェクト
     const [isSubmit, setIsSubmit] = useState(false);
+    //ログイン状態を判別するステート
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
 
     const navigate = useNavigate();
+
+    //ログイン情報をサーバーに送信し、アクセストークンを取得する関数
+    async function login(username, password) {
+        const response = await axios.post('http://127.0.0.1:8080/token', qs.stringify({
+            "username": username,
+            "password": password,
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        const { access_token } = response.data;
+        return access_token;
+    };
+
+    //アクセストークンをローカルストレージに保存する関数
+    function saveToken(token) {
+        localStorage.setItem('access_token', token);
+    }
+
+    async function getUserId(username) {
+        try {
+            const response = await axios.get('http://127.0.0.1:8080/user/');
+            const users = response.data;
+
+            const user = users.find(user => user.name === username);
+
+            if (user) {
+                return user.id;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     //フォームに入力された値を取り出す関数
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormValues({ ...formValues, [name]: value });
     };
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        //ログイン情報を発信する。
         //バリデーションチェックする
         setFormErrors(validate(formValues));
         setIsSubmit(true);
         setFormValues({});
-    };
+        //エラーメッセージがなければログイン情報を発信する。
+        if (Object.keys(formErrors).length === 0) {
+            const token = await login(formValues.username, formValues.password);
+            saveToken(token);
+            setIsLoggedIn(true);
+            getUserId(formValues.username).then(id => {
+                if (id) {
+                    console.log(`User ID is: ${id}`);
+                    navigate('/dashboard/' + id.toString());
+                } else {
+                    console.log('No user with the name "string" was found.');
+                }
+            });
+        };
+    }
 
     const validate = (values) => {
         const errors = {};
@@ -37,6 +93,15 @@ const Loginform = () => {
         };
         return errors;
     };
+
+    // ページロード時に既存のトークンをチェック
+    useEffect(() => {
+        const token = localStorage.getItem('access_token');
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
     return (
         <div class="log">
             <div class="loginform-background">
@@ -55,7 +120,7 @@ const Loginform = () => {
                         </div>
                         <p className="errorMsg">{formErrors.password}</p>
                         {/* <button class="submitButton" onClick={(e) => handleSubmit(e)}>login</button> */}
-                        <button onClick={() => navigate('/dashboard/1')}>login</button>
+                        <button onClick={(e) => handleSubmit(e)}>login</button>
                     </div>
                 </div>
             </div >
