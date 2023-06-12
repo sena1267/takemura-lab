e1b16b9b27291a7a6b125168675b9a65c439c166
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-06-12 13:14:20 +0900

    add current and target to backend

--- backend/src/models.py
+++ backend/src/models.py
@@ -21,6 +21,8 @@ class User(Base):
     birthday = Column(Integer)
     is_admin = Column(Boolean)
     at_office = Column(Boolean)
+    current = Column(Integer)
+    target = Column(Integer)
 
 
 class Equipment(Base):
