6f2c9616c55ecb21160a9fd5b5d4d9b3d723f3b0
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-06-17 22:42:00 +0900

    add backend sec

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,26 @@
 from database import database
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from routers import (user, equipment, equipment_lack, auth, information)
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import get_swagger_ui_html
+
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = "hirofuru"
+    correct_password = "@pp"
+    if credentials.username != correct_username or credentials.password != correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 database.Base.metadata.create_all(bind=database.engine)
 
-app = FastAPI()
+app = FastAPI(docs_url=None)  # Disable default docs
 
 app.include_router(auth.router)
 app.include_router(user.router)
@@ -21,3 +36,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+@app.get("/docs", dependencies=[Depends(get_current_username)], include_in_schema=False)
+async def get_documentation():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title=app.title)
