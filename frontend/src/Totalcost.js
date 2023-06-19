d6ff1c82c59397c4fe3ac5d14f293f03ce75d0ac
kyutech-tatsuma <131667347+kyutech-tatsuma@users.noreply.github.com>
2023-06-19 22:02:42 +0900

    Update Totalcost.js

--- frontend/src/Totalcost.js
+++ frontend/src/Totalcost.js
@@ -13,7 +13,7 @@ const Totalcost = ({ totalcost }) => {
                             <div className="col mr-2">
                                 <div className="text-xs font-weight-bold text-success text-uppercase mb-1 font-japanese">
                                     総備品コスト</div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">{totalcost ? `￥${totalcost}` : '0'}</div>
+                                <div className="h5 mb-0 font-weight-bold text-gray-800">{totalcost ? `￥${totalcost}` : '￥0'}</div>
                             </div>
                             <div className="col-auto">
                                 <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
